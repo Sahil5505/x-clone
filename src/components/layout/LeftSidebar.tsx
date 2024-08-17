7ada79d813ab81cef6f6cadcdcd66f8292a1d177
sahil5505 <sahilrevankar20co21@gmail.com>
2024-08-18 00:43:34 +0530

    setting up database

--- src/components/layout/LeftSidebar.tsx
+++ src/components/layout/LeftSidebar.tsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 export default function LeftSidebar() {
     return (
     <aside className="hidden w-1/4 overscroll-y-auto border-r border-gray-200 p-6 md:sticky md:block h-screen xl:w-1/5">
-        <Link className="mb-8" href="/">
+        <Link className="mb-8 block" href="/">
             <h1 className="h-8 w-8 text-gray-800">
             𝕏
             </h1>
