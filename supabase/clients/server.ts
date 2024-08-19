86122b341d5866d9b804a0f2f682aa262b6b3699
sahil5505 <sahilrevankar20co21@gmail.com>
2024-08-19 16:43:08 +0530

    checkpoint>>!

---
+++ supabase/clients/server.ts
@@ -0,0 +1,37 @@
+import { createServerClient as creteSupabaseServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export function createReadOnlyServerClient() {
+    const cookieStore = cookies()
+    return creteSupabaseServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,   
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies:{
+                getAll() {
+                    return cookieStore.getAll();
+                },
+            }
+        }
+    )
+}
+
+export function createServerClient() {
+    const cookieStore = cookies()
+    return creteSupabaseServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,   
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies:{
+                getAll() {
+                    return cookieStore.getAll();
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options}) =>
+                    cookieStore.set(name, value, options),
+                    );
+                },
+            },
+        },
+    );
+}
